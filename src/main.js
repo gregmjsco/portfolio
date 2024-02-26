6e406528ca1062ee63717a8a31a9fd64ff58b180
Greg Johnston <gregmj.sco@gmail.com>
2024-02-26 21:58:51 +0900

    Add contact section to html and create contact.css

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import '../styles/components/header.css';
 import '../styles/components/hero.css';
 import '../styles/components/about.css';
 import '../styles/components/featured.css';
+import '../styles/components/contact.css'
 import '../styles/utils.css';
 
 
