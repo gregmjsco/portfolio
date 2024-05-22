8bbbd463e67326e6223c3ffe4bae20c57b10c59a
Greg Johnston <gregmj.sco@gmail.com>
2024-05-22 18:54:52 +0900

    Add simple footer

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import '../styles/components/hero.css';
 import '../styles/components/about.css';
 import '../styles/components/featured.css';
 import '../styles/components/contact.css'
+import '../styles/components/footer.css'
 import '../styles/utils.css';
 
 
