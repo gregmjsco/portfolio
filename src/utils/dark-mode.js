5dbb48dc41716e8469b090bae7ea6025874a324b
Greg Johnston <gregmj.sco@gmail.com>
2024-05-27 06:47:51 +0900

    Darkmode button toggle working but some styles need tweaked for light mode

--- src/utils/dark-mode.js
+++ src/utils/dark-mode.js
@@ -1,1 +1,12 @@
-const themeToggleBtns = document.querySelectorAll('#theme-toggle')
+const darkMode = () => {
+    const themeToggleBtns = document.querySelectorAll('#theme-toggle')
+    
+    themeToggleBtns.forEach(btn => {
+        btn.addEventListener('click', () => {
+            document.body.classList.toggle('light-mode');
+        })
+    })
+}
+
+
+export default darkMode;
