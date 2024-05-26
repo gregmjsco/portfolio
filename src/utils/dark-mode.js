27e50fe1e6d5fa2738c14099217eb5340a32bc48
Greg Johnston <gregmj.sco@gmail.com>
2024-05-27 07:08:49 +0900

    Add theme to local storage and fix button text color in light mode

--- src/utils/dark-mode.js
+++ src/utils/dark-mode.js
@@ -1,9 +1,18 @@
 const darkMode = () => {
     const themeToggleBtns = document.querySelectorAll('#theme-toggle')
+
+    const theme = localStorage.getItem('theme');
+
+    theme && document.body.classList.add(theme);
     
     themeToggleBtns.forEach(btn => {
         btn.addEventListener('click', () => {
             document.body.classList.toggle('light-mode');
+            if(document.body.classList.contains('light-mode')){
+                localStorage.setItem('theme', 'light-mode');
+            } else {
+                localStorage.removeItem('theme');
+            }
         })
     })
 }
